39c72ce8d0d1710e1aaf5f2e4b97d1c78b11e72c
Aditya Verma <aditya17009@iiitd.ac.in>
2018-10-19 12:10:32 +0530

    Added Game Class

---
+++ src/Game.java
@@ -0,0 +1,9 @@
+public class Game {
+    public final static int GRID_ROWS = 8;
+    public final static int GRID_COLS = 5;
+    public final static double TILE_SIZE = 75;
+
+    public Game() {
+
+    }
+}
