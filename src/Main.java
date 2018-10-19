eeb9e629ee1d32147754459876a50cc73c445eda
Aditya Verma <aditya17009@iiitd.ac.in>
2018-10-19 10:35:48 +0530

    Added bare bones JavaFx code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public final static double SCREEN_WIDTH = 400;
+    public final static double SCREEN_HEIGHT = 640;
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        // set the window title and disable resizing
+        primaryStage.setTitle("Snake vs Blocks");
+        primaryStage.setResizable(false);
+
+        // avoid the unnecessary padding on right and bottom
+        primaryStage.sizeToScene();
+
+        // initialize the root node
+        Group root = new Group();
+
+        // initialize the canvas
+        Canvas canvas = new Canvas(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+
+        // add canvas to root node
+        root.getChildren().add(canvas);
+
+        // initialize the scene
+        Scene scene = new Scene(root);
+
+        // set the scene and show the stage
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
     }
 }
