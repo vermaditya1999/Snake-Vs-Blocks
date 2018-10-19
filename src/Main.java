56e15d6a0feaadb908408322a2f2d1a42291cdc7
Aditya Verma <aditya17009@iiitd.ac.in>
2018-10-19 13:55:31 +0530

    Add animation loop

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.geometry.VPos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -34,6 +39,31 @@ public class Main extends Application {
         // set the scene and show the stage
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        // start the animation loop
+        animationLoop(canvas.getGraphicsContext2D());
+    }
+
+    private void animationLoop(GraphicsContext gc) {
+
+        // initialize a Game object
+        Game game = new Game(gc);
+
+        // set some global attributes
+        gc.setTextAlign(TextAlignment.CENTER);
+        gc.setTextBaseline(VPos.CENTER);
+        gc.setStroke(Color.WHITE);
+
+        // initialize AnimationTimer
+        AnimationTimer animationTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+
+            }
+        };
+
+        // star the animation timer
+        animationTimer.start();
     }
 
     public static void main(String[] args) {
