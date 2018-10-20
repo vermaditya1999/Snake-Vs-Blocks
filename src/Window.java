48d7da8341674fc013c795fe0ecbf959f070603d
Aditya Verma <aditya17009@iiitd.ac.in>
2018-10-20 17:24:05 +0530

    Added abstract Window class

---
+++ src/Window.java
@@ -0,0 +1,36 @@
+import javafx.geometry.VPos;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.text.TextAlignment;
+
+abstract public class Window {
+
+    private Canvas canvas;
+    private GraphicsContext gc;
+
+    public Window() {
+        // Initialize the canvas
+        canvas = new Canvas(Game.SCREEN_WIDTH, Game.SCREEN_HEIGHT);
+
+        // Initialize the GraphicsContext
+        gc = canvas.getGraphicsContext2D();
+
+        // Enable KeyEvent detection
+        canvas.setFocusTraversable(true);
+
+        // Load defaults
+        loadDefaults();
+
+        // Add Event Handlers
+        addEventHandlers();
+    }
+
+    private void loadDefaults() {
+
+        // Set Text Align and Baseline to CENTER
+        gc.setTextAlign(TextAlignment.CENTER);
+        gc.setTextBaseline(VPos.CENTER);
+    }
+
+    abstract protected void addEventHandlers();
+}
