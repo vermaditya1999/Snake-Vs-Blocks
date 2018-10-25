9d21e7e9d1cbcc5d42e087ff5a1246b2d799f849
Aditya Verma <aditya17009@iiitd.ac.in>
2018-10-26 01:05:00 +0530

    Changed Tile size from 75 to 76

--- src/Game.java
+++ src/Game.java
@@ -8,7 +8,7 @@ public class Game implements WindowController {
     public final static int GRID_ROWS = 8;
     public final static int GRID_COLS = 5;
 
-    public final static double TILE_SIZE = 75;
+    public final static double TILE_SIZE = 76;
 
     public final static double SCREEN_WIDTH = Game.TILE_SIZE * Game.GRID_COLS;
     public final static double SCREEN_HEIGHT = Game.TILE_SIZE * Game.GRID_ROWS;
