299385cda9a88b27e55e0f387c562d5efb76748e
Aditya Verma <aditya17009@iiitd.ac.in>
2018-10-31 17:31:16 +0530

    Tweaked graphics and improved logic

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -7,6 +7,8 @@ import javafx.scene.text.Font;
 
 public class Menu extends Window {
 
+    public static final Color BG_COLOR = Color.rgb(79,53,88);
+
     private MenuButton pgBtn;
     private MenuButton lbBtn;
 
@@ -32,10 +34,12 @@ public class Menu extends Window {
             } else {
 
                 if (lbBtn.isHovered(mouseX, mouseY)) {
+                    resetMouseVars();
                     windowController.setWindow(Windows.LeaderBoard);
                 }
 
                 if (pgBtn.isHovered(mouseX, mouseY)) {
+                    resetMouseVars();
                     windowController.setWindow(Windows.GamePlay);
                 }
             }
@@ -54,25 +58,37 @@ public class Menu extends Window {
         });
     }
 
+    private void resetMouseVars() {
+        mouseX = 0;
+        mouseY = 0;
+    }
+
     @Override
     public void show() {
 
+        // Set hover properties of buttons
+        pgBtn.setHovered(pgBtn.isHovered(mouseX, mouseY));
+        lbBtn.setHovered(lbBtn.isHovered(mouseX, mouseY));
+
         // Set mouse pointer
-        if (lbBtn.isHovered(mouseX, mouseY) || pgBtn.isHovered(mouseX, mouseY)) {
+        if (pgBtn.isHovered(mouseX, mouseY) || lbBtn.isHovered(mouseX, mouseY)) {
             canvas.setCursor(Cursor.HAND);
         } else {
             canvas.setCursor(Cursor.DEFAULT);
         }
 
         // Set background
-        gc.setFill(Color.rgb(79,53,88));
+        gc.setFill(Menu.BG_COLOR);
         gc.fillRect(0, 0, Game.SCREEN_WIDTH, Game.SCREEN_HEIGHT);
 
-        // Game name
-        gc.setFont(new Font("Consolas", 60));
+        // Display Game title
         gc.setFill(Color.WHITE);
-        gc.fillText("Snake", Game.SCREEN_WIDTH / 2, Game.TILE_SIZE);
+
+        gc.setFont(new Font("Consolas", 75));
         gc.fillText("Vs", Game.SCREEN_WIDTH / 2, Game.TILE_SIZE * 1.75);
+
+        gc.setFont(new Font("Consolas", 65));
+        gc.fillText("Snake", Game.SCREEN_WIDTH / 2, Game.TILE_SIZE);
         gc.fillText("Blocks", Game.SCREEN_WIDTH / 2, Game.TILE_SIZE * 2.5);
 
         // LeaderBoard button
--- src/main/java/MenuButton.java
+++ src/main/java/MenuButton.java
@@ -11,6 +11,8 @@ public class MenuButton {
     private double height;
     private double width;
 
+    private boolean hovered;
+
     {
         height = Game.TILE_SIZE / 2;
         width = 3 * Game.TILE_SIZE;
@@ -22,15 +24,31 @@ public class MenuButton {
     }
 
     public void show(GraphicsContext gc) {
-        gc.setStroke(Color.WHITE);
 
-        gc.strokeRect(Game.SCREEN_WIDTH / 2 - width / 2, posY - height / 2, width, height);
+        if (hovered) {
+            gc.setFill(Color.WHITE);
+            gc.fillRect(Game.SCREEN_WIDTH / 2 - width / 2, posY - height / 2, width, height);
+        } else {
+            gc.setLineWidth(2.0);
+            gc.setStroke(Color.WHITE);
+            gc.strokeRect(Game.SCREEN_WIDTH / 2 - width / 2, posY - height / 2, width, height);
+        }
+
         gc.setFont(new Font("Consolas", 20));
-        gc.setFill(Color.WHITE);
+
+        if (hovered) {
+            gc.setFill(Menu.BG_COLOR);
+        } else {
+            gc.setFill(Color.WHITE);
+        }
         gc.fillText(text, Game.SCREEN_WIDTH / 2, posY);
     }
 
     public boolean isHovered(double mouseX, double mouseY) {
         return Math.abs(mouseX - Game.SCREEN_WIDTH / 2) <= width / 2 && Math.abs(mouseY - posY) <= height / 2;
+    }
+
+    public void setHovered(boolean hovered) {
+        this.hovered = hovered;
     }
 }
