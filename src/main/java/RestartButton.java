c291b60ba580824c1597c193b84b495d370beb4f
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-01 09:43:12 +0530

    Add RestartButton class

---
+++ src/main/java/RestartButton.java
@@ -0,0 +1,24 @@
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class RestartButton {
+
+    private Vector pos;
+    private Image image;
+    private double size;
+
+    {
+        pos = new Vector(Game.SCREEN_WIDTH / 2, Game.SCREEN_HEIGHT / 2 + Game.TILE_SIZE * 2.5);
+        image = new Image("restart.png");
+        size = Game.TILE_SIZE;
+    }
+
+    public void show(GraphicsContext gc) {
+
+        gc.drawImage(image, pos.x - size / 2, pos.y - size / 2, size, size);
+    }
+
+    public boolean isHovered(double mouseX, double mouseY) {
+        return Math.abs(mouseX - pos.x) <= size / 2 && Math.abs(mouseY - pos.y) <= size / 2;
+    }
+}
