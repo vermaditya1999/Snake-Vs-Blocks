4fa1e4f05e0ac146e938676276c8452ad069d1e7
Vinayak <vinayak17122@iiitd.ac.in>
2018-11-01 17:37:49 +0530

    Add update function in Wall class
    Signed-off-by: Vinayak <vinayak17122@iiitd.ac.in>

--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -5,18 +5,21 @@ public class Wall {
 
     private Vector pos;
 
-    private float length;
+    private final double length = Game.TILE_SIZE;
 
-    public Wall (double x, double y, float length){
+    public Wall (double x, double y){
 
-        this.length = length;
-        pos = new Vector(x, y);
+        pos = new Vector((x - 1) * Game.TILE_SIZE + Game.TILE_SIZE / 2, (y - 1) * Game.TILE_SIZE + Game.TILE_SIZE / 2);
     }
 
     public void show(GraphicsContext gc){
 
         gc.setFill(Color.WHITE);
-        gc.setLineWidth(5);
+        gc.setLineWidth(4);
         gc.strokeLine(pos.x, pos.y, pos.x, pos.y + length );
+    }
+
+    public void update(double speed) {
+        pos.y += speed;
     }
 }
