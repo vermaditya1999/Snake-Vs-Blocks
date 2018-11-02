a0c1cce9a44661eb27d6c1a704f24f5147b7ce66
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-02 11:41:06 +0530

    Add random color

--- src/main/java/Particle.java
+++ src/main/java/Particle.java
@@ -1,7 +1,12 @@
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+import java.util.Random;
+
 public class Particle {
+
+    private static Random random = new Random();
+
     private Vector pos;
     private Vector vel;
     private Vector acc;
@@ -9,15 +14,23 @@ public class Particle {
     private double radius;
     private double alpha;
 
+    private int r;
+    private int g;
+    private int b;
+
     public Particle(double x, double y) {
         pos = new Vector(x, y);
         vel = Vector.random();
         acc = vel.copy();
         acc.mult(-0.01);
-        vel.mult(4);
+        vel.mult(5);
 
-        radius = 5;
+        radius = 6;
         alpha = 1;
+
+        r = random.nextInt(256);
+        g = random.nextInt(256);
+        b = random.nextInt(256);
     }
 
     public boolean isDead() {
@@ -28,11 +41,12 @@ public class Particle {
         vel.add(acc);
         pos.add(vel);
 
-        radius -= 0.25;
-        alpha -= 0.025;
+        radius -= 0.35;
+//        alpha -= 0.025;
     }
 
     public void show(GraphicsContext gc) {
+//        gc.setFill(Color.rgb(r, g, b, alpha));
         gc.setFill(Color.rgb(255, 255, 255, alpha));
         gc.fillOval(pos.x, pos.y, radius * 2, radius * 2);
     }
