5b715ae6a529903366f3e4230bbba45dbe2500e9
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-02 11:40:22 +0530

    Add isOVer and update methods

--- src/main/java/Token.java
+++ src/main/java/Token.java
@@ -2,15 +2,26 @@ import javafx.scene.canvas.GraphicsContext;
 
 public abstract class Token {
 
-    protected float radius = 20;
+    protected float radius;
 
     protected Vector pos;
 
+    {
+        radius = 10;
+    }
+
     public Token (double x, double y){
 
-        pos = new Vector(x, y);
+        pos = new Vector((x - 1) * Game.TILE_SIZE + Game.TILE_SIZE / 2, (y - 1) * Game.TILE_SIZE + Game.TILE_SIZE / 2);
     }
 
     public abstract void show(GraphicsContext gc);
 
+    public void update(double speed) {
+        pos.y += speed;
+    }
+
+    public boolean isOver() {
+        return pos.y - radius >= Game.SCREEN_HEIGHT;
+    }
 }
