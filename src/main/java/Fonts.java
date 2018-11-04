661ff9d06290d8dc80cc6905871f03ff702c6ed3
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-04 22:49:48 +0530

    Add Fonts class

---
+++ src/main/java/Fonts.java
@@ -0,0 +1,9 @@
+import javafx.scene.text.Font;
+
+public class Fonts {
+
+    public static final Font GOTHAM_LARGE = Font.loadFont(Main.class.getClassLoader().getResourceAsStream("fonts/Gotham-Light.otf"), 65);
+    public static final Font GOTHAM_MEDIUM = Font.loadFont(Main.class.getClassLoader().getResourceAsStream("fonts/Gotham-Light.otf"), 45);
+    public static final Font GOTHAM_SMALL = Font.loadFont(Main.class.getClassLoader().getResourceAsStream("fonts/Gotham-Light.otf"), 25);
+    public static final Font MUSEO = Font.loadFont(Main.class.getClassLoader().getResourceAsStream("fonts/Museo-100.otf"), 60);
+}
