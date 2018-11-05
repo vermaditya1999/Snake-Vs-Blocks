c9073363f738b3f21ad574077a5b88ff9fff5549
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-05 21:25:14 +0530

    Rename currentWindow to getCurrentWindow
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

--- src/main/java/WindowController.java
+++ src/main/java/WindowController.java
@@ -3,7 +3,7 @@ import javafx.event.Event;
 public interface WindowController {
     void setWindow(Windows window);
 
-    Windows currentWindow();
+    Windows getCurrentWindow();
 
     void passEvent(Windows window, Event event);
 }
