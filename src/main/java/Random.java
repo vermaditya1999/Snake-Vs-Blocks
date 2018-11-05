51ffcccc594a8d19893c4d1f608c3ee344e8bfd1
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-05 22:05:38 +0530

    Add static nextDouble method
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

--- src/main/java/Random.java
+++ src/main/java/Random.java
@@ -1,10 +1,17 @@
 public class Random {
     public static java.util.Random random;
 
-    public int nextInt(int n) {
+    public static int nextInt(int n) {
         if (random == null) {
             random = new java.util.Random();
         }
         return random.nextInt(n);
+    }
+
+    public static double nextDouble() {
+        if (random == null) {
+            random = new java.util.Random();
+        }
+        return random.nextDouble();
     }
 }
