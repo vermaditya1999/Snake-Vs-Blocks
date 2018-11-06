e6257e7e6cdbdb14ec8236c5aab7cb9e3b380add
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-06 20:52:51 +0530

    Add consume field and isConsumed method and refactor isOver method isDead
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

--- src/main/java/com/snakevsblocks/Token.java
+++ src/main/java/com/snakevsblocks/Token.java
@@ -8,12 +8,12 @@ public abstract class Token implements Collideable {
 
     public Vector pos;
 
-    public boolean dead;
+    public boolean consumed;
 
     public Token(double x, double y) {
 
         pos = new Vector((x - 1) * App.TILE_SIZE + App.TILE_SIZE / 2, (y - 1) * App.TILE_SIZE + App.TILE_SIZE / 2);
-        dead = false;
+        consumed = false;
     }
 
     public abstract void show(GraphicsContext gc);
@@ -22,12 +22,16 @@ public abstract class Token implements Collideable {
         pos.y += speed;
     }
 
-    public boolean isOver() {
-        return dead || (pos.y - Token.RADIUS >= App.SCREEN_HEIGHT);
+    public boolean isDead() {
+        return pos.y - Token.RADIUS >= App.SCREEN_HEIGHT;
+    }
+
+    public boolean isConsumed() {
+        return consumed;
     }
 
     @Override
     public void collide(Vector snakeHeadVector) {
-        dead = Vector.dist(snakeHeadVector, pos) <= (SnakeBall.RADIUS + Token.RADIUS);
+        consumed = Vector.dist(snakeHeadVector, pos) <= (SnakeBall.RADIUS + Token.RADIUS);
     }
 }
