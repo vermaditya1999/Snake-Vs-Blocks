f99fb8756de686644a547ee238dd45a83ffdb13b
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-06 21:26:28 +0530

    Make Burst class abstract
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

--- src/main/java/com/snakevsblocks/Burst.java
+++ src/main/java/com/snakevsblocks/Burst.java
@@ -4,11 +4,11 @@ import javafx.scene.canvas.GraphicsContext;
 
 import java.util.ArrayList;
 
-public class Burst {
-    private ArrayList<Particle> particles;
-    private Vector origin;
-    private int maxNum;
-    private int curNum;
+public abstract class Burst {
+    protected ArrayList<Particle> particles;
+    protected Vector origin;
+    protected int maxNum;
+    protected int curNum;
 
     public Burst(double x, double y) {
         particles = new ArrayList<Particle>();
@@ -23,21 +23,5 @@ public class Burst {
         return particles.size() == 0;
     }
 
-    public void show(GraphicsContext gc) {
-        if (curNum < maxNum) {
-            particles.add(new Particle(origin.x, origin.y));
-            curNum++;
-        }
-
-        for (int i = particles.size() - 1; i >= 0; i--) {
-            Particle p = particles.get(i);
-
-            p.update();
-            if (p.isDead()) {
-                particles.remove(i);
-            } else {
-                p.show(gc);
-            }
-        }
-    }
+    public abstract void show(GraphicsContext gc);
 }
