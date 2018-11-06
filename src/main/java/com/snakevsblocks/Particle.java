0232f7f58490b4b0b8169f861a27b3ff1f1e3a62
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-06 21:30:54 +0530

    Make Particle class abstract
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

--- src/main/java/com/snakevsblocks/Particle.java
+++ src/main/java/com/snakevsblocks/Particle.java
@@ -1,34 +1,21 @@
 package com.snakevsblocks;
 
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
 
-public class Particle {
+public abstract class Particle {
 
     private Vector pos;
     private Vector vel;
-    private Vector acc;
 
     private double radius;
     private double alpha;
 
-    private int r;
-    private int g;
-    private int b;
-
     public Particle(double x, double y) {
         pos = new Vector(x, y);
         vel = Vector.random();
-        acc = vel.copy();
-//        acc.mult(-0.01);
         vel.mult(5);
-
         radius = 6;
         alpha = 1;
-
-        r = Random.nextInt(256);
-        g = Random.nextInt(256);
-        b = Random.nextInt(256);
     }
 
     public boolean isDead() {
@@ -36,16 +23,10 @@ public class Particle {
     }
 
     public void update() {
-//        vel.add(acc);
         pos.add(vel);
-
         radius -= 0.25;
         alpha -= 0.025;
     }
 
-    public void show(GraphicsContext gc) {
-//        gc.setFill(Color.rgb(r, g, b, alpha));
-        gc.setFill(Color.rgb(255, 255, 255, alpha));
-        gc.fillOval(pos.x - radius, pos.y - radius, radius * 2, radius * 2);
-    }
+    public abstract void show(GraphicsContext gc);
 }
