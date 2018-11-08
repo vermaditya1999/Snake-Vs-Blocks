487d1ec4311e7ede26763d46a98314e93ac033d5
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-08 12:16:29 +0530

    Remove attractable ability
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

--- src/main/java/com/snakevsblocks/Coin.java
+++ src/main/java/com/snakevsblocks/Coin.java
@@ -2,24 +2,15 @@ package com.snakevsblocks;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 
 public class Coin extends Token {
-
-    public static boolean attractable = false;
 
     public Coin(int x, int y) {
         super(x, y);
     }
 
-    public void update(double speed, Vector snakeHeadVec) {
+    public void update(double speed) {
         pos.y += speed;
-
-        if (attractable && pos.y >= snakeHeadVec.y) {
-            Vector dir = Vector.sub(snakeHeadVec, pos);
-            dir.setMag(35);
-            pos.add(dir);
-        }
     }
 
     @Override
@@ -29,11 +20,5 @@ public class Coin extends Token {
         // To be changed later
         gc.setFill(Color.YELLOW);
         gc.fillOval(pos.x - Token.RADIUS, pos.y - Token.RADIUS, 2 * Token.RADIUS, 2 * Token.RADIUS);
-
-        // Display text
-        gc.setFill(Color.WHITE);
-        gc.setFont(new Font("Consolas", 15));
-        gc.fillText("Coin", pos.x, pos.y + 2.5 * Token.RADIUS);
-
     }
 }
