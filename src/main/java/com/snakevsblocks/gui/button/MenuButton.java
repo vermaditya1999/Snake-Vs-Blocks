70988c2c5eea9c035c6f8e3cd9843f80e165d95a
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-09 23:44:22 +0530

    MenuButton now extends Button
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

--- src/main/java/com/snakevsblocks/gui/button/MenuButton.java
+++ src/main/java/com/snakevsblocks/gui/button/MenuButton.java
@@ -5,29 +5,29 @@ import com.snakevsblocks.util.Font;
 import com.snakevsblocks.window.Menu;
 import javafx.scene.canvas.GraphicsContext;
 
-public class MenuButton {
+public class MenuButton extends Button {
 
     private static final double height = App.TILE_SIZE / 2;
     private static final double width = App.TILE_SIZE * 3;
 
     private String text;
-    private double posY;
     private boolean hovered;
 
     public MenuButton(String text, double posY) {
+        super(App.SCREEN_WIDTH / 2, posY);
         this.text = text;
-        this.posY = posY;
     }
 
+    @Override
     public void show(GraphicsContext gc) {
 
         // Show button
         gc.setLineWidth(2.0);
         gc.setStroke(Menu.FG_COLOR);
-        gc.strokeRect(App.SCREEN_WIDTH / 2 - width / 2, posY - height / 2, width, height);
+        gc.strokeRect(App.SCREEN_WIDTH / 2 - width / 2, pos.y - height / 2, width, height);
         if (hovered) {
             gc.setFill(Menu.FG_COLOR);
-            gc.fillRect(App.SCREEN_WIDTH / 2 - width / 2, posY - height / 2, width, height);
+            gc.fillRect(App.SCREEN_WIDTH / 2 - width / 2, pos.y - height / 2, width, height);
         }
 
         // Set font
@@ -39,10 +39,11 @@ public class MenuButton {
         } else {
             gc.setFill(Menu.FG_COLOR);
         }
-        gc.fillText(text, App.SCREEN_WIDTH / 2, posY);
+        gc.fillText(text, App.SCREEN_WIDTH / 2, pos.y);
     }
 
+    @Override
     public boolean isHovered(double mouseX, double mouseY) {
-        return hovered = Math.abs(mouseX - App.SCREEN_WIDTH / 2) <= width / 2 && Math.abs(mouseY - posY) <= height / 2;
+        return hovered = Math.abs(mouseX - App.SCREEN_WIDTH / 2) <= width / 2 && Math.abs(mouseY - pos.y) <= height / 2;
     }
 }
