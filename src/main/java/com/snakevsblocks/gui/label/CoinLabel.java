902853724149772d19f5f2e8224c5f42de7e224d
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-10 18:39:46 +0530

    Adjust Text Align
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

--- src/main/java/com/snakevsblocks/gui/label/CoinLabel.java
+++ src/main/java/com/snakevsblocks/gui/label/CoinLabel.java
@@ -1,11 +1,13 @@
 package com.snakevsblocks.gui.label;
 
 import com.snakevsblocks.App;
+import com.snakevsblocks.entity.SnakeBall;
 import com.snakevsblocks.entity.token.Token;
 import com.snakevsblocks.util.Font;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 
 public class CoinLabel extends Label {
 
@@ -23,15 +25,20 @@ public class CoinLabel extends Label {
 
     @Override
     public void show(GraphicsContext gc) {
+
+        gc.setTextAlign(TextAlignment.LEFT);
+
         gc.setFont(Font.CONSOLAS_MEDIUM);
         gc.setFill(Color.WHITE);
-        gc.fillText(text, pos.x, pos.y + 1);
+        gc.fillText(text, pos.x + App.TILE_SIZE / 4 + (Token.RADIUS - SnakeBall.RADIUS), pos.y + 1);
+
+        gc.setTextAlign(TextAlignment.CENTER);
 
         if (image == null) {
             gc.setFill(Color.YELLOW);
             gc.fillOval(pos.x - Token.RADIUS, pos.y - Token.RADIUS, 2 * Token.RADIUS, 2 * Token.RADIUS);
         } else {
-            gc.drawImage(image, pos.x - App.TILE_SIZE / 2 - Token.RADIUS,
+            gc.drawImage(image, pos.x - Token.RADIUS,
                     pos.y - Token.RADIUS, 2 * Token.RADIUS, 2 * Token.RADIUS);
         }
     }
--- src/main/java/com/snakevsblocks/gui/label/ScoreLabel.java
+++ src/main/java/com/snakevsblocks/gui/label/ScoreLabel.java
@@ -1,8 +1,11 @@
 package com.snakevsblocks.gui.label;
 
+import com.snakevsblocks.App;
+import com.snakevsblocks.entity.SnakeBall;
 import com.snakevsblocks.util.Font;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 
 public class ScoreLabel extends Label {
     public ScoreLabel(double x, double y) {
@@ -11,8 +14,16 @@ public class ScoreLabel extends Label {
 
     @Override
     public void show(GraphicsContext gc) {
+
+        gc.setTextAlign(TextAlignment.LEFT);
+
         gc.setFont(Font.CONSOLAS_MEDIUM);
         gc.setFill(Color.WHITE);
-        gc.fillText(text, pos.x, pos.y + 1);
+        gc.fillText(text, pos.x + App.TILE_SIZE / 4, pos.y + 1);
+
+        gc.setTextAlign(TextAlignment.CENTER);
+
+        gc.setFill(SnakeBall.COLOR);
+        gc.fillOval(pos.x - SnakeBall.RADIUS, pos.y - SnakeBall.RADIUS, 2 * SnakeBall.RADIUS, 2 * SnakeBall.RADIUS);
     }
 }
