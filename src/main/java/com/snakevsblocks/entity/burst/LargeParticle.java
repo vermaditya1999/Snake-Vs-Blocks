e6f39fab736af2f988ba4c82e017bef7182020c5
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-10 17:58:57 +0530

    Assign color to Particles
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

--- src/main/java/com/snakevsblocks/entity/burst/LargeParticle.java
+++ src/main/java/com/snakevsblocks/entity/burst/LargeParticle.java
@@ -7,13 +7,12 @@ public class LargeParticle extends Particle {
 
     public LargeParticle(double x, double y) {
         super(x, y);
-
         radius = 7;
     }
 
     @Override
     public void show(GraphicsContext gc) {
-        gc.setFill(Color.rgb(255, 255, 255, alpha));
+        gc.setFill(Color.rgb(color[0], color[1], color[2], alpha));
         gc.fillRect(pos.x - radius, pos.y - radius, radius * 2, radius * 2);
     }
 }
--- src/main/java/com/snakevsblocks/entity/burst/SmallParticle.java
+++ src/main/java/com/snakevsblocks/entity/burst/SmallParticle.java
@@ -7,13 +7,12 @@ public class SmallParticle extends Particle {
 
     public SmallParticle(double x, double y) {
         super(x, y);
-
         radius = 6;
     }
 
     @Override
     public void show(GraphicsContext gc) {
-        gc.setFill(Color.rgb(255, 255, 255, alpha));
+        gc.setFill(Color.rgb(color[0], color[1], color[2], alpha));
         gc.fillOval(pos.x - radius, pos.y - radius, radius * 2, radius * 2);
     }
 }
