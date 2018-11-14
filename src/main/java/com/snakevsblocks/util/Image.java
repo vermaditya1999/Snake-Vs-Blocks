6e04777c8ecba8f6e74b1a24584b2ac46d4d9c24
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-14 10:00:50 +0530

    Make fields static private and add getters
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

--- src/main/java/com/snakevsblocks/util/Image.java
+++ src/main/java/com/snakevsblocks/util/Image.java
@@ -2,12 +2,40 @@ package com.snakevsblocks.util;
 
 public class Image {
 
-    public static final javafx.scene.image.Image COIN = new javafx.scene.image.Image("img/coin.png");
-    public static final javafx.scene.image.Image DESTROYER = new javafx.scene.image.Image("img/destroyer.png");
-    public static final javafx.scene.image.Image MAGNET = new javafx.scene.image.Image("img/magnet.png");
-    public static final javafx.scene.image.Image SHIELD = new javafx.scene.image.Image("img/shield.png");
+    private static javafx.scene.image.Image COIN = new javafx.scene.image.Image("img/coin.png");
+    private static javafx.scene.image.Image DESTROYER = new javafx.scene.image.Image("img/destroyer.png");
+    private static javafx.scene.image.Image MAGNET = new javafx.scene.image.Image("img/magnet.png");
+    private static javafx.scene.image.Image SHIELD = new javafx.scene.image.Image("img/shield.png");
 
-    public static final javafx.scene.image.Image B_BACK = new javafx.scene.image.Image("img/back-black.png");
-    public static final javafx.scene.image.Image W_BACK = new javafx.scene.image.Image("img/back-white.png");
-    public static final javafx.scene.image.Image RESTART = new javafx.scene.image.Image("img/restart.png");
+    private static javafx.scene.image.Image B_BACK = new javafx.scene.image.Image("img/back-black.png");
+    private static javafx.scene.image.Image W_BACK = new javafx.scene.image.Image("img/back-white.png");
+    private static javafx.scene.image.Image RESTART = new javafx.scene.image.Image("img/restart.png");
+
+    public static javafx.scene.image.Image getCOIN() {
+        return COIN;
+    }
+
+    public static javafx.scene.image.Image getDESTROYER() {
+        return DESTROYER;
+    }
+
+    public static javafx.scene.image.Image getMAGNET() {
+        return MAGNET;
+    }
+
+    public static javafx.scene.image.Image getSHIELD() {
+        return SHIELD;
+    }
+
+    public static javafx.scene.image.Image getBBack() {
+        return B_BACK;
+    }
+
+    public static javafx.scene.image.Image getWBack() {
+        return W_BACK;
+    }
+
+    public static javafx.scene.image.Image getRESTART() {
+        return RESTART;
+    }
 }
