72c338f1c7218dc34efd48e97bc182951e1e849b
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-14 09:44:41 +0530

    Add wrapper Image class
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

---
+++ src/main/java/com/snakevsblocks/util/Image.java
@@ -0,0 +1,13 @@
+package com.snakevsblocks.util;
+
+public class Image {
+
+    public static final javafx.scene.image.Image COIN = new javafx.scene.image.Image("img/coin.png");
+    public static final javafx.scene.image.Image DESTROYER = new javafx.scene.image.Image("img/destroyer.png");
+    public static final javafx.scene.image.Image MAGNET = new javafx.scene.image.Image("img/magnet.png");
+    public static final javafx.scene.image.Image SHIELD = new javafx.scene.image.Image("img/shield.png");
+
+    public static final javafx.scene.image.Image B_BACK = new javafx.scene.image.Image("img/back-black.png");
+    public static final javafx.scene.image.Image W_BACK = new javafx.scene.image.Image("img/back-white.png");
+    public static final javafx.scene.image.Image RESTART = new javafx.scene.image.Image("img/restart.png");
+}
