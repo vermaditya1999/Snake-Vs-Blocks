60ffcd61ff1b9a91e1703c1fcce6aab943d4c99c
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-14 10:06:18 +0530

    Remove Image field and make show method abstract
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

--- src/main/java/com/snakevsblocks/gui/button/BackButton.java
+++ src/main/java/com/snakevsblocks/gui/button/BackButton.java
@@ -2,23 +2,18 @@ package com.snakevsblocks.gui.button;
 
 import com.snakevsblocks.App;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 
 abstract public class BackButton extends Button {
 
-    private Image image;
     private double size;
 
-    public BackButton(double x, double y, String url) {
+    public BackButton(double x, double y) {
         super(x, y);
-        image = new Image(url);
         size = App.TILE_SIZE / 2;
     }
 
     @Override
-    public void show(GraphicsContext gc) {
-        gc.drawImage(image, pos.x - size / 2, pos.y - size / 2, size, size);
-    }
+    abstract public void show(GraphicsContext gc);
 
     @Override
     public boolean isHovered(double mouseX, double mouseY) {
