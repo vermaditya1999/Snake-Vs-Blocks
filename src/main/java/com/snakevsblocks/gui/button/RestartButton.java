920fc88c393d714ea3d0e57aa5486d211da68827
Aditya Verma <aditya17009@iiitd.ac.in>
2018-11-14 10:18:41 +0530

    Use Image class image in Restart button
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

--- src/main/java/com/snakevsblocks/gui/button/RestartButton.java
+++ src/main/java/com/snakevsblocks/gui/button/RestartButton.java
@@ -1,23 +1,21 @@
 package com.snakevsblocks.gui.button;
 
 import com.snakevsblocks.App;
+import com.snakevsblocks.util.Image;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 
 public class RestartButton extends Button {
 
-    private Image image;
     private double size;
 
     public RestartButton(double x, double y) {
         super(x, y);
-        image = new Image("img/restart.png");
         size = App.TILE_SIZE;
     }
 
     @Override
     public void show(GraphicsContext gc) {
-        gc.drawImage(image, pos.x - size / 2, pos.y - size / 2, size, size);
+        gc.drawImage(Image.getRESTART(), pos.x - size / 2, pos.y - size / 2, size, size);
     }
 
     @Override
