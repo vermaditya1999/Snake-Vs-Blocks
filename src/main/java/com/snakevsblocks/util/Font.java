6d2b779ab740b70949007ccca0197d1ebf101775
Aditya Verma <aditya17009@iiitd.ac.in>
2018-12-02 10:02:31 +0530

    Add Consolas.ttf and use it in Font class
    Signed-off-by: Aditya Verma <aditya17009@iiitd.ac.in>

--- src/main/java/com/snakevsblocks/util/Font.java
+++ src/main/java/com/snakevsblocks/util/Font.java
@@ -6,10 +6,10 @@ public class Font {
     public static final javafx.scene.text.Font GOTHAM_MEDIUM = javafx.scene.text.Font.loadFont(Font.class.getClassLoader().getResourceAsStream("fonts/Gotham-Light.otf"), 45);
     public static final javafx.scene.text.Font GOTHAM_SMALL = javafx.scene.text.Font.loadFont(Font.class.getClassLoader().getResourceAsStream("fonts/Gotham-Light.otf"), 25);
 
-    public static final javafx.scene.text.Font CONSOLAS_LARGE = new javafx.scene.text.Font("Consolas", 50);
-    public static final javafx.scene.text.Font CONSOLAS_MEDIUM = new javafx.scene.text.Font("Consolas", 30);
-    public static final javafx.scene.text.Font CONSOLAS_SMALL = new javafx.scene.text.Font("Consolas", 20);
-    public static final javafx.scene.text.Font CONSOLAS_XSMALL = new javafx.scene.text.Font("Consolas", 15);
+    public static final javafx.scene.text.Font CONSOLAS_LARGE = javafx.scene.text.Font.loadFont(Font.class.getClassLoader().getResourceAsStream("fonts/Consolas.ttf"), 50);
+    public static final javafx.scene.text.Font CONSOLAS_MEDIUM = javafx.scene.text.Font.loadFont(Font.class.getClassLoader().getResourceAsStream("fonts/Consolas.ttf"), 30);
+    public static final javafx.scene.text.Font CONSOLAS_SMALL = javafx.scene.text.Font.loadFont(Font.class.getClassLoader().getResourceAsStream("fonts/Consolas.ttf"), 20);
+    public static final javafx.scene.text.Font CONSOLAS_XSMALL = javafx.scene.text.Font.loadFont(Font.class.getClassLoader().getResourceAsStream("fonts/Consolas.ttf"), 15);
 
     public static final javafx.scene.text.Font MUSEO = javafx.scene.text.Font.loadFont(Font.class.getClassLoader().getResourceAsStream("fonts/Museo-100.otf"), 60);
 }
